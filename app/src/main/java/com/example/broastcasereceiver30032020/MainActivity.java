6be422fa2d2896d1f406327b97b7528a3e0048e4
phatdroid94 <phatdroid94@gmail.com>
2020-05-25 20:33:53 +0700

    check connection

--- app/src/main/java/com/example/broastcasereceiver30032020/MainActivity.java
+++ app/src/main/java/com/example/broastcasereceiver30032020/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.broastcasereceiver30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Cach 1 : Dang ky nhan broadcast theo vong doi activity
+        IntentFilter intent = new IntentFilter();
+        intent.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(broadcastReceiver, intent);
     }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager cm = (ConnectivityManager)
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            if (networkInfo != null){
+                if (networkInfo.isConnected() && networkInfo.isAvailable()) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        Toast.makeText(context, "Da ket noi wifi", Toast.LENGTH_SHORT).show();
+                    }
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE){
+                        Toast.makeText(context, "Da ket noi 3g", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    };
+
 }
